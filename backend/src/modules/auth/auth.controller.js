fc1f3af63a3c519c21efe40173103e2be212b14e
huzaifaali2111 <mianhuzaifaali62@gmail.com>
2026-05-11 22:31:08 +0500

    Add data validation middleware and integrate with user signup route
    - Introduced a new validation middleware in validation.middleware.js to handle request body validation using Zod.
    - Updated the user signup route in auth.routes.js to utilize the new validation middleware for improved error handling.
    - Removed outdated validation logic from auth.controller.js to streamline the signup process.

--- backend/src/modules/auth/auth.controller.js
+++ backend/src/modules/auth/auth.controller.js
@@ -1,29 +1,9 @@
 import express from "express";
 import * as z from "zod";
-import userValidation from "./auth.validation.js";
+
 
 async function userSignup(req, res) {
     const { firstName, lastName, email, password, confirmPassword } = req.body
-
-
-
-    const result = userValidation.safeParse(req.body);
-
-    const errors = {};
-
-    result.error.issues.forEach((issue) => {
-        const key = issue.path.join(".");
-
-        if (!errors[key]) {
-            errors[key] = [];
-        }
-
-        errors[key].push(issue.message);
-    });
-
-    return res.status(400).json({
-        errors,
-    });
 
     return res.status(201).json({
         message: "signup",
--- backend/src/modules/auth/auth.routes.js
+++ backend/src/modules/auth/auth.routes.js
@@ -1,10 +1,12 @@
 import express from "express";
 import authController from "./auth.controller.js";
+import userValidation from "./auth.validation.js";
+import validate from "../../core/middleware/validation.middleware.js";
 const router = express.Router();
 
 
 // signup 
-router.post("/signup",authController.userSignup);
+router.post("/signup",validate(userValidation)authController.userSignup);
 
 // login 
 router.post("/login", (req, res) => {
